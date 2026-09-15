613c3f7b593180c1a512daa697e2035893afbe04
kazim <kazimasar6@gmail.com>
2026-09-15 07:03:58 +0000

    container config done

--- .devcontainer/basic/01_varable.js
+++ .devcontainer/basic/01_varable.js
@@ -2,6 +2,7 @@ const account = 12345
 let accountEmail = "kazimasar@gmail.com"
 var accountPassword = "24325"
 accounCity = "jaipur"
+let accountState;
 
 
 // accountId = 2
@@ -11,5 +12,5 @@ accounCity = "karbal"
 
 
 
-console.table([accountEmail,accountPassword,accounCity])
+console.table([accountEmail,accountPassword,accounCity,accountState])
 // clurly barcket is used for ol programmer in  old year and many programmar easily cahnge the var therefore the problem face to the  prograamer then they are not use in now  a day so we cannot use the var in our javascript code
